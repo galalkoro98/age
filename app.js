51af8a76208b7e5a6f83a3240767f04c26442b8a
Galalkoro98 <galal.koro@gmail.com>
2022-10-04 23:54:06 +0200

    fixed and updated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let age;
 if (input < 18 || input >= 65) {
     age = 'you are not alow to drink alcohols';
 } else if (input > 18) {
-    age = `<img>'you are alow to drink alcohols';</img>`
+    age = 'you are allow to drink alcohols'
 } else if (input == 18) {
     age = 'you may start to drink alcohols';   
 } else {
