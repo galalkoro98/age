cb665bc6b8db385f1a73bbe8ff61ff60af828dac
Galalkoro98 <galal.koro@gmail.com>
2022-10-04 19:55:13 +0200

    update

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ const input = prompt('enter your age')
 
 let age;
 
-if (input < 18 && input === 0 || input > 65) {
+if (input < 18 || input >= 65) {
     age = 'you are not alow to drink alcohols';
 } else if (input > 18) {
-    age = 'you are alow to drink alcohols';
-} else if (input === 18) {
-    age = 'you may start to drink alcohols \n but is not good for you';   
-} else if (input >= 0 || input <= ' '){
-    age = 'you enter zero  or you did not enter nothing';
+    age = `<img>'you are alow to drink alcohols';</img>`
+} else if (input == 18) {
+    age = 'you may start to drink alcohols';   
+} else {
+    age = 'you enter nothing';
 }
-
 const result = `${age}`;
 //console.log(result);
 alert(result);
